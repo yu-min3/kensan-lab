47a02ace4620c0163576bb67b28cc7f24bbd7e7d
yu-min <ymisaki00@gmail.com>
2025-11-10 14:00:21 +0000

    templatesの修正。必要な環境変数の設定

--- backstage-app/yarn.sh
+++ backstage-app/yarn.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Yarn wrapper script for easier command execution
 # Usage: ./yarn.sh <command> [args...]
+export NODE_OPTIONS="--no-node-snapshot"
 exec node .yarn/releases/yarn-4.4.1.cjs "$@"
